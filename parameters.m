3c461e2ef918b2af1811d0e1fce2954faedc5a27
Emir <aliemirkaplan49@gmail.com>
2026-06-18 21:32:46 +0300

    pre kalman commit
    working electrical + mechanical model. implemented anti windup pi controller and current + speed controllers.

--- parameters.m
+++ parameters.m
@@ -3,7 +3,7 @@ Ld = 0.002;      % d-axis Inductance (H)
 Lq = 0.003;      % q-axis Inductance (H)
 lambda_m = 0.05; % PM Flux Linkage (Wb) // lamda_f
 N = 4;           % Pole Pairs
-J_sum = 0.001;       % Rotor Inertia (kg*m^2) // J
+J_m = 0.001;       % Rotor Inertia (kg*m^2) // J, J_sum
 Bm = 0.0005;     % Damping Coefficient
 
 % DC Bus Voltage (Standard 48V system for robotics)
@@ -12,3 +12,10 @@ Vdc = 48;
 % Switching Frequency (10 kHz)
 f_sw = 10000;
 Ts = 1/f_sw;
+
+% aerospace actuator update
+T_dist = 0.1; % Nm step at t = 0.5s
+N_gear = 50;
+J_load = 0.01;
+J_sum = J_m + J_load/(N_gear^2);
+K_hinge = 0.2; % Nm/rad
